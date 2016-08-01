1799b189a80d0f32b94eae4b3146829eede11f13
FaisalBinAhmed <faisalbin@outlook.com>
2016-08-02 01:05:56 +0600

    changed rss and css for grid UI

--- rss.php
+++ rss.php
@@ -1,26 +1,42 @@
 <?php
+echo "<link rel='stylesheet' type='text/css' href='style.css' />";
+
 $html = "";
-$feeds = array("http://anandtech.com/rss", "http://arstechnica.com/feed", "http://www.dhakatribune.com/rss/latest");
+$feeds = array("http://anandtech.com/rss", "http://arstechnica.com/feed");
+
+echo "<div class=\"grid-container outline\">";
+
 foreach ($feeds as $url) {
   # code...
+echo "<div class=\"row\">";
+
 
 $xml = simplexml_load_file($url);
-for($i = 0; $i < 3; $i++){
+for($i = 0; $i < 2; $i++){
+	$j=$i+1;
+
 	$title = $xml->channel->item[$i]->title;
 	$link = $xml->channel->item[$i]->link;
 //  $author = $xml->channel->$item[$i]->author;
-	$description = $xml->channel->item[$i]->description;
+	//$description = $xml->channel->item[$i]->description;
 	$pubDate = $xml->channel->item[$i]->pubDate;
 //  $media = $xml->channel->$item[$i]->thumbnail;
 //  $thumb = $media->thumbnail->attributes();
 //  $murl = (string) $thumb['url'];
-
+				$html .= "<div class=\"col-$j\">";
         $html .= "<a href='$link'><h3>$title</h3></a>";
       //  $html .= "<br />$author<hr />";
       //  $html .= "<img src=$media>";
-      	$html .= "$description";
+      //	$html .= "$description";
       	$html .= "<br />$pubDate<hr />";
 
-}}
+$html .= "</div>";
+}
+
+echo "</div>";
+
+}
 echo $html;
+echo "</div>";
+
 ?>
