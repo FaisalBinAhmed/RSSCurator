b76262782b0804a2ff28e4afb784b7f3657b7f7b
FaisalBinAhmed <faisalbin@outlook.com>
2016-08-07 23:59:10 +0600

    fixes

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<a href="setting.php"><input class="submit text-input" type="button" name="home" value="Home" ></a>
+<a href="rss.php"><input class="submit text-input" type="button" name="home" value="Home" ></a>
 <a href="profile.php"><input class="submit text-input" type="button" name="profile" value="profile"></a>
 <a href="logout.php"><input class="submit text-input" type="button" name="logout" value="Sign Out" ></a><br>
 
