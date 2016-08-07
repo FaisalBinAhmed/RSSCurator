50c2f50242b7816194a4d9416ef1225929cfa0f2
istiakMahbub <fahim_aiub22@yahoo.com>
2016-08-07 13:24:23 +0600

    login updated

--- login.php
+++ login.php
@@ -9,7 +9,10 @@
             echo "database not selected";
         }   
 
-        $email=$_POST['loginEmail'];
+        session_start();
+        if (isset($_POST['btnLogin']))
+        {
+            $email=$_POST['loginEmail'];
         $pass=$_POST['loginPass'];
 
        
@@ -17,13 +20,31 @@
         $result=mysqli_query($con,$sql) or die(mysqli_error());
         
         $srch= mysqli_fetch_array($result);
+
         if($srch['email']==$email && $srch['password']==$pass){
-            echo "Welcome ".$srch['email'];
+            $name=$srch['name'];
+            $uid=$srch['UID'];
+            $fonts=$srch['fontsize'];
+            $bgcolor=$srch['bgcolor'];
+
+            $_SESSION['valid'] = true;
+          $_SESSION['timeout'] = time();
+            $_SESSION['email'] = $email;
+            $_SESSION['UID'] = $uid;
+            $_SESSION['name'] = $name;
+            $_SESSION['password'] = $pass;
+            $_SESSION['fontsize'] = $fonts;
+            $_SESSION['bgcolor'] = $bgcolor;
+            
+        
+                  
+            //echo 'You have entered valid use name and password';
+            header('Location: profile.php');
         }
         else{
-            echo "login hoi nai ";
+
+            header('Location: register.php?login=false');
         }
-
-
+    }
  ?>
  
