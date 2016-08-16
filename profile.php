b0a0d29af9f93d6bed76908d75ef8538fefa0044
istiakMahbub <fahim_aiub22@yahoo.com>
2016-08-17 03:59:24 +0600

    profile updated
    Login information solved ,

--- profile.php
+++ profile.php
@@ -7,13 +7,11 @@
 </head>
 <body>
 
-<?php	include('navigation.php'); ?>
-<!--
-<a href="rss.php"><input class="submit text-input" type="button" name="home" value="Home" ></a>
+<a href="setting.php"><input class="submit text-input" type="button" name="home" value="Home" ></a>
 <a href="profile.php"><input class="submit text-input" type="button" name="profile" value="profile"></a>
 <a href="logout.php"><input class="submit text-input" type="button" name="logout" value="Sign Out" ></a><br>
 
--->
+
 <?php
 session_start();
 
@@ -49,9 +47,9 @@ echo "Font: ".$_SESSION["font"]."<br>" ;
 	<input type="submit" name="updateEmail" value="update Email"/><br>
 
 	 <select name="font">
-  <option value="segoe">Segoe UI</option>
-  <option value="times">Times Roman</option>
-  <option value="verdana">Verdana</option>
+  <option value="Segoe UI">Segoe UI</option>
+  <option value="Times Roman">Times Roman</option>
+  <option value="Verdana">Verdana</option>
   <input type="submit" name="updateFont" value="update Font"/><br>
 </select>
 
@@ -60,36 +58,138 @@ Font Size:
 	<input type="submit" name="updateFS" value="update Font Size"/><br>
 <hr>
 
- 	<input type="checkbox" name="check_list[]" value="1">Entertainment<br>
-  	<input type="checkbox" name="check_list[]" value="2">Sports<br>
-  	<input type="checkbox" name="check_list[]" value="3">Technology<br>
-  	<input type="checkbox" name="check_list[]" value="4">Politics<br>
-  	<input type="checkbox" name="check_list[]" value="5">World<br>
-  	<input type="checkbox" name="check_list[]" value="6">Local<br>
-  	<input type="checkbox" name="check_list[]" value="7">Science<br>
-  	<input type="checkbox" name="check_list[]" value="8">Top<br>
-  	<input type="submit" value="Submit" name="catSel">
+    <?php
+
+    $con=mysqli_connect('localhost','root','');
+
+    if(!$con)
+    {
+        echo "Not connected";
+    }
+    if(!mysqli_select_db($con,'rsscurator'))
+    {
+        echo "database not selected";
+    }
+
+
+
+
+$q2 = "SELECT catname FROM categories WHERE catID IN ( SELECT catID from usercats WHERE uid =".$_SESSION["UID"]." )";
+$result2 = mysqli_query($con,$q2) or die( mysqli_error($con) );
+$rows2 = array();
+$index2 = 0;
+while($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC))
+{
+    $rows2[$index2] = $row2["catname"];
+        $index2++;
+}
+        
+
+        echo "<select name='categories'>";
+        if (!in_array("Entertainment", $rows2))
+        {
+             echo "<option value='1'>Entertainment</option>" ;
+        }
+        
+        if (!in_array("Sports", $rows2))
+        {
+            echo "<option value='2'>Sports</option>";
+        }
+        if (!in_array("Technology", $rows2))
+        {
+            echo "<option value='3'>Technology</option>";
+        }
+        if (!in_array("Politics", $rows2))
+        {
+            echo "<option value='4'>Politics</option>";
+        }
+        if (!in_array("World", $rows2))
+        {
+            echo "<option value='5'>World</option>";
+        }
+        if (!in_array("Local", $rows2))
+        {
+            echo "<option value='6'>Local</option>";
+        }
+        if (!in_array("Science", $rows2))
+        {
+            echo "<option value='7'>Science</option>";
+        }
+        if (!in_array("Top", $rows2))
+        {
+            echo "<option value='8'>Top</option>";
+        }
+     
+        echo "<input type='submit' value='Submit' name='catSel'>"; echo "<br>";
+      
+        echo "</select>";
+    ?>
+
+<select name="categoriesDel">
+  <option value="1">Entertainment</option>
+  <option value="2">Sports</option>
+  <option value="3">Technology</option>
+  <option value="4">Politics</option>
+  <option value="5">World</option>
+  <option value="6">Local</option>
+  <option value="7">Science</option>
+  <option value="8">Top</option>
+  <input type="submit" value="Delete" name="catDel"><br>
+</select>
+
 </form>
 </body>
 </html>
 
 <?php
 
-	$con=mysqli_connect('localhost','root','');
 
-	if(!$con)
-	{
-		echo "Not connected";
-	}
-	if(!mysqli_select_db($con,'rsscurator'))
-	{
-		echo "database not selected";
-	}
+
+   
+
+
+	
+
+if(!empty($rows2[0]))
+{
+    echo $rows2[0]."\n";
+    
+}
+if(!empty($rows2[1]))
+{
+    echo $rows2[1]."\n";
+}
+if(!empty($rows2[2]))
+{
+    echo $rows2[2]."\n";
+}
+if(!empty($rows2[3]))
+{
+    echo $rows2[3]."\n";
+}
+if(!empty($rows2[4]))
+{
+    echo $rows2[4]."\n";
+}
+if(!empty($rows2[5]))
+{
+    echo $rows2[5]."\n";
+}
+if(!empty($rows2[6]))
+{
+    echo $rows2[6]."\n";
+}
+if(!empty($rows2[7]))
+{
+    echo $rows2[7]."\n";
+}
+
 
 
 	 if (isset($_POST['updateName']))
 	 {
     	$newName = $_POST['newName'];
+        $_SESSION["name"]=$newName;
     	$sql= "UPDATE users SET name='$newName' WHERE UID='$uid'";
 
         if(!mysqli_query($con, $sql))
@@ -102,10 +202,42 @@ Font Size:
         }
 	}
 
+    else if (isset($_POST['catSel']))
+     {
+        $cat = $_POST['categories'];
+        $sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '$cat')";
+
+        if(!mysqli_query($con, $sql))
+        {
+            echo "Not Updated";
+        }
+        else
+        {
+            echo "Updated";
+        }
+    }
+
+     else if (isset($_POST['catDel']))
+     {
+        $cat = $_POST['categoriesDel'];
+        $sql= "DELETE FROM usercats WHERE UID='$uid' AND catID='$cat'";
+
+        if(!mysqli_query($con, $sql))
+        {
+            echo "Not Deleted";
+        }
+        else
+        {
+            echo "Deleted";
+        }
+    }
+
 	else if (isset($_POST['updatePass']))
 	 {
     	$oldpass = $_POST['oldpass'];
 		$newpass = $_POST['newpass'];
+
+        $_SESSION["password"]=$newpass;
     	$sql= "UPDATE users SET password='$newpass' WHERE UID='$uid'";
 
         if(!mysqli_query($con, $sql))
@@ -121,6 +253,7 @@ Font Size:
 	else if (isset($_POST['updateEmail']))
 	 {
     	$newEmail = $_POST['newEmail'];
+        $_SESSION["email"]=$newEmail;
     	$sql= "UPDATE users SET email='$newEmail' WHERE UID='$uid'";
 
         if(!mysqli_query($con, $sql))
@@ -136,6 +269,7 @@ Font Size:
 	else if (isset($_POST['updateFont']))
 	 {
     	$newFont = $_POST['font'];
+        $_SESSION["font"]=$newFont;
     	$sql= "UPDATE users SET font='$newFont' WHERE UID='$uid'";
 
         if(!mysqli_query($con, $sql))
@@ -151,6 +285,7 @@ Font Size:
 	else if (isset($_POST['updateFS']))
 	 {
     	$newFS = $_POST['newFS'];
+        $_SESSION["fontsize"]=$newFS;
     	$sql= "UPDATE users SET fontsize='$newFS' WHERE UID='$uid'";
 
         if(!mysqli_query($con, $sql))
@@ -162,63 +297,5 @@ Font Size:
             echo "Updated";
         }
 	}
-
-
-	if(!empty($_POST['check_list']))
-	{
-		foreach($_POST['check_list'] as $check)
-		{
-    	  	if ($check == 1)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '1')";
-				mysqli_query($con, $sql);
-
-        	}
-        	else if ($check == 2)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '2')";
-		        mysqli_query($con, $sql);
-
-        	}
-
-        	else if ($check == 3)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '3')";
-				mysqli_query($con, $sql);
-
-        	}
-        	else if ($check == 4)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '4')";
-		        mysqli_query($con, $sql);
-
-        	}
-
-        	else if ($check == 5)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '5')";
-				mysqli_query($con, $sql);
-
-        	}
-        	else if ($check == 6)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '6')";
-		        mysqli_query($con, $sql);
-
-        	}
-
-        	else if ($check == 7)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '7')";
-				mysqli_query($con, $sql);
-
-        	}
-        	else if ($check == 8)
-        	{
-        		$sql= "INSERT INTO usercats (UID,catID) VALUES ('$uid', '8')";
-		        mysqli_query($con, $sql);
-
-        	}
-		}
-    }
+    
  ?>
--- register.php
+++ register.php
@@ -11,21 +11,13 @@
     <form action="login.php" method="post">
          <p style="color:white">Already have an account?</p>
 
-<<<<<<< HEAD
-            <input class="login-form" type="email" name="loginEmail" placeholder="Registerd Email">
-
-            <input class="login-form" type="password" name="loginPass" placeholder="Password">
-
-            <button class="login-form" type="submit" name="btnLogin" value="Sign IN"><b>Sign In</b></button>
-=======
             <input class="login-form" type="email" name="loginEmail" placeholder="Registerd Email" required>
         
             <input class="login-form" type="password" name="loginPass" placeholder="Password" required>
         
             <button class="login-submit" type="submit" name="btnLogin" value="Sign IN"><b>Sign In</b></button>
->>>>>>> origin/master
     </form>
-
+    
 </div>
 
 
@@ -33,22 +25,22 @@
 <div class="signUP">
             
     <form action="config.php" method="post">
-
-
+       
+            
         <input class="text-input" id="name" type="text" name="name" placeholder="Full Name" required maxlength="22"><br>
-
-
+        
+ 
 
         <input class="text-input" type="email" name="email" placeholder="Email" required><br>
-
-
+  
+    
         <input class="text-input"  type="password" name="pass" placeholder="Password length between 5 to 10" required pattern=".{5,10}"><br>
 
-
+ 
         <input class="text-input" type="password"  name="conPass" placeholder="Confirm Password" required pattern=".{5,10}"><br>
 
         <button class="submit text-input" type="submit" value="Sign up"><b>Sign Up</b></button><br>
-
+                     
     </form>
  </div>
 
